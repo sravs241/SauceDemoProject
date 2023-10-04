196c53c84ac6718bb1e3eead03298009c34613ed
srava <srava@sravs>
2023-10-04 15:25:31 +0530

    first commit...

---
+++ src/main/java/com/SauceDemoProject/com/SauceDemoProject/App.java
@@ -0,0 +1,13 @@
+package com.SauceDemoProject.com.SauceDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
